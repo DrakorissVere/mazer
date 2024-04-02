4ebe2e19db1bcb74659c04d7c024c77ff6dda9ad
Drakoriss <anmunimei@gmail.com>
2024-04-02 13:32:28 +0300

    add window creation

---
+++ src/main.py
@@ -0,0 +1,32 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+    def __init__(self, width, height):
+        self.root = Tk()
+        self.root.title("a-mazer")
+        self.canvas = Canvas(self.root, width=width, height=height)
+        self.canvas.pack()
+        self.is_running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+
+    def wait_for_close(self):
+        self.is_running = True
+        while self.is_running:
+            self.redraw()
+
+    def close(self):
+        self.is_running = False
+
+
+def main():
+    window = Window(800, 600)
+    window.wait_for_close()
+
+
+if __name__ == '__main__':
+    main()
