0987ea92102ff167d7b35167fea50b24f85efa26
Drakoriss <anmunimei@gmail.com>
2024-04-03 15:28:02 +0300

    add Point and Line classes

--- src/main.py
+++ src/main.py
@@ -1,12 +1,33 @@
 from tkinter import Tk, BOTH, Canvas
 
+# x=0 is the left side of the screen
+# y=0 is the top of the screen
+
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def draw(self, canvas, fill_color="white"):
+        canvas.create_line(self.a.x, self.a.y, self.b.x,
+                           self.b.y, fill=fill_color, width=2)
+        canvas.pack(fill=BOTH, expand=1)
+
 
 class Window:
     def __init__(self, width, height):
         self.root = Tk()
         self.root.title("a-mazer")
-        self.canvas = Canvas(self.root, width=width, height=height)
-        self.canvas.pack()
+        self.canvas = Canvas(self.root, width=width,
+                             height=height, bg="#272822")
+        self.canvas.pack(fill=BOTH, expand=1)
         self.is_running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -22,9 +43,14 @@ class Window:
     def close(self):
         self.is_running = False
 
+    def draw_line(self, line, fill_color="white"):
+        line.draw(self.canvas, fill_color)
+
 
 def main():
     window = Window(800, 600)
+    line = Line(Point(100, 100), Point(200, 200))
+    window.draw_line(line)
     window.wait_for_close()
 
 
